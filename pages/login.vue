fc88041cc5333ea1cff66c70ce1fe954b073fbe6
Cesar Solano <csolanoc@unal.edu.co>
2022-06-10 19:56:48 -0500

    signup page

--- pages/login.vue
+++ pages/login.vue
@@ -30,63 +30,6 @@
       </v-col>
     </div>
   </v-container>
-  <!-- <v-container class="fill-height" fluid>
-    <v-row align="center" justify="center" dense>
-      <v-col cols="12" sm="8" md="4" lg="4">
-        <v-card elevation="0" class="primary">
-          <div class="text-center">
-            <h1 class="mb-2">Log in</h1>
-          </div>
-          <v-img class="mx-auto" src="/login.png" />
-
-          <v-card-text>
-            <v-form>
-              <v-text-field
-                label="Enter your username"
-                name="username"
-                prepend-inner-icon="mdi-account"
-                type="text"
-                class="rounded-0"
-                background-color="primary"
-                outlined
-                color="light"
-                v-model="username"
-              ></v-text-field>
-              <v-text-field
-                label="Enter your password"
-                name="password"
-                prepend-inner-icon="mdi-lock"
-                type="password"
-                background-color="primary"
-                class="rounded-0"
-                outlined
-                color="light"
-                v-model="password"
-              ></v-text-field>
-              <v-btn
-                class="rounded-0 accent"
-                color="#000000"
-                x-large
-                block
-                dark
-                @click="login"
-                >Login
-              </v-btn>
-              <v-card-actions class="justify-center text-subtitle-2">
-                Don't have an account yet?
-                <NuxtLink
-                  class="pl-2 ml-6"
-                  style="color: #ffffff"
-                  to="/register"
-                  >Sign Up
-                </NuxtLink>
-              </v-card-actions>
-            </v-form>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container> -->
 </template>
 
 <script>
--- pages/register.vue
+++ pages/register.vue
@@ -1,10 +1,128 @@
 <template>
-    <Register />
+  <!-- <Register /> -->
+  <v-container fluid>
+    <div class="d-flex align-center justify-center mt-16">
+      <v-col class="bg d-flex flex-column justify-center">
+        <h1 class="font-italic login-text white--text font-weight-medium pl-16">
+          Datos de usuario
+        </h1>
+        <v-col class="login-items pl-16" cols="10">
+          <v-text-field
+            name="nombre"
+            label="Nombre"
+            prepend-icon="mdi-account"
+            solo
+            rounded
+            dense
+          ></v-text-field>
+          <v-text-field
+            name="email"
+            label="Email"
+            prepend-icon="mdi-email"
+            solo
+            rounded
+            dense
+          ></v-text-field>
+          <v-text-field
+            name="cel"
+            label="Teléfono"
+            prepend-icon="mdi-cellphone"
+            solo
+            rounded
+            dense
+          ></v-text-field>
+          <v-text-field
+            name="cargo"
+            label="Cargo"
+            prepend-icon="mdi-account-details"
+            solo
+            rounded
+            dense
+          ></v-text-field>
+          <v-text-field
+            name="password"
+            label="Contraseña"
+            prepend-icon="mdi-lock"
+            solo
+            rounded
+            dense
+          ></v-text-field>
+          <v-text-field
+            name="passwordConfirm"
+            label="Confirmar contraseña"
+            prepend-icon="mdi-lock"
+            solo
+            rounded
+            dense
+          ></v-text-field>
+        </v-col>
+      </v-col>
+      <v-col class="second-bg d-flex flex-column" cols="6">
+        <h1 class="font-italic login-text2">Datos de la empresa</h1>
+        <v-col class="login-items login-items-2" cols="10">
+          <v-text-field
+            name="nombreEmpresa"
+            label="Nombre de la empresa"
+            prepend-icon="mdi-domain"
+            filled
+          ></v-text-field>
+          <v-text-field
+            name="tipoEmpresa"
+            label="Tipo de la empresa"
+            prepend-icon="mdi-apps"
+            filled
+          ></v-text-field>
+          <v-btn color="red" class="send-btn white--text font-italic"
+            >Enviar</v-btn
+          >
+        </v-col>
+      </v-col>
+    </div>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: 'RegisterPage',
-  
 }
 </script>
+
+<style scoped>
+.login-text {
+  /* margin-bottom: 100px; */
+}
+
+.login-text2 {
+  margin-top: 40px;
+  margin-left: 50px;
+  padding-bottom: 50px;
+}
+
+.login-items-2 {
+  margin-left: 50px;
+  /* margin-bottom: 121px; */
+  padding-bottom: 50px;
+}
+
+.login-items {
+  /* padding-top: 100px; */
+  /* padding-bottom: 150px; */
+  /* padding-left: 100px; */
+}
+
+.send-btn {
+  margin-left: 350px;
+  margin-top: 120px;
+}
+.bg {
+  height: 500px;
+  background: #d72428;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25), 0px 4px 4px rgba(0, 0, 0, 0.25);
+  /* border-radius: 15px; */
+}
+
+.second-bg {
+  background: center / contain no-repeat url('/bg-register.png');
+  /* background-size: cover; */
+}
+</style>
