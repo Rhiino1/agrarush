7d15cf4321d1feb4ad08e6b69406aae3487b3f5e
Cesar Solano <csolanoc@unal.edu.co>
2022-06-14 00:11:37 -0500

    config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,8 @@
 export default {
-  // target: 'static',
-  router: {
-    // base: '/agrarush/'
-  },
+  target: 'static',
+  // router: {
+  //   base: '/agrarush/'
+  // },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - AgraRush',
